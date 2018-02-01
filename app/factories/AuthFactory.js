01cc3ee64205d4045ff51bcb355ee5b5163312ba
Jordan Castelloe <jordan.castelloe@gmail.com>
2018-02-01 13:57:58 -0600

    commented authfacotry and filterfactory

--- app/factories/AuthFactory.js
+++ app/factories/AuthFactory.js
@@ -1,1 +1,15 @@
 'use strict'; 
+
+// this is copied and pasted from Joe's auth example. We can do whatever we want with it but here it is for reference!
+
+
+// function authUser(){
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   return firebase
+//     .auth()
+//     .signInWithPopup(provider);
+// };
+
+// function logout() {
+//  return firebase.auth().signOut();
+// }
--- app/factories/FilterFactory.js
+++ app/factories/FilterFactory.js
@@ -1,2 +1,9 @@
 'use strict'; 
 angular.module('');
+
+// Joe's code, copied and pasted - do what you want with it but here it is for reference!
+// angular.module("TodoApp").factory("FilterFactory", function () {
+//   return {
+//     searchTerm: ""
+//   };
+// });
