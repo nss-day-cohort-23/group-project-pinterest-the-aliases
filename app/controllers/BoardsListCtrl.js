6758c11df7eae1b1718f5fb8bb07c6f9edfcb968
Valerie Freeman <valerah7@gmail.com>
2018-02-02 10:47:05 -0600

    created boards list page

--- app/controllers/BoardsListCtrl.js
+++ app/controllers/BoardsListCtrl.js
@@ -5,6 +5,55 @@ angular.module("Winterest").controller("BoardsListCtrl", function ($scope) {
 
 // DISPLAY BOARDS
 //call Image factory to get all boards, then set boards as a scope variable
+    
+    //Dummy data
+    $scope.boards = [
+        {
+            id: "board1",
+            title: "Dogs",
+            description: "Dogs I think are cute"
+        },
+        {
+            id: "board2",
+            title: "Cats",
+            description: "Cats I think are stupid"
+        },
+        {
+            id: "board3",
+            title: "Nicolas Cage",
+            description: "Faces of Nicolas Cage"
+        },
+        {
+            id: "board4",
+            title: "NSS",
+            description: "All the things at NSS"
+        },
+        {
+            id: "board5",
+            title: "Food",
+            description: "Yummy food I like"
+        },
+        {
+            id: "board6",
+            title: "TV Shows",
+            description: "My favorite bits"
+        },
+        {
+            id: "board7",
+            title: "hair",
+            description: "Hairstyles to avoid"
+        },
+        {
+            id: "board8",
+            title: "funny",
+            description: "Funny pics"
+        },
+        {
+            id: "board9",
+            title: "Losers",
+            description: "Lame people"
+        }
+    ];
 
 // LOAD INDIVIDUAL BOARD WHEN YOU CLICK ON A BOARD
 // grab board id
