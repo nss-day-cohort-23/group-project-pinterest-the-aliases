762bc21b28b924789ba8a5e6c3e7073a0bb4826d
Hunter Phillips <hkphillips42@Hunters-MacBook-Pro.local>
2018-02-02 13:55:30 -0600

    hp-rando

--- app/controllers/ImgListCtrl.js
+++ app/controllers/ImgListCtrl.js
@@ -2,11 +2,20 @@
 
 angular.module("Winterest").controller("ImgListCtrl", function ($scope, ImgFactory) {
     $scope.title = "Image List";
+    //shuffle array to display random images
+    const shuffleArr = (array) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    };
+
     // DISPLAY ALL IMAGES
     // call image factory and get all images, set image array to scope variable
     ImgFactory.getAllImages()
     .then(data => {
-        $scope.images = data;
+        $scope.images = shuffleArr(data);
     });
 
     // GET ALL OF THE USER'S BOARDS --> this will be in the modal
