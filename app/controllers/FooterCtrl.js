62ec4c2f106737a50ce20e1733098a4818623451
Eli Jackson <tucker.eli.jackson@gmail.com>
2018-02-02 15:44:55 -0600

    added footer with links and a footer ctrl that runs the function to take you to the top of the page

---
+++ app/controllers/FooterCtrl.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module("Winterest").controller("FooterCtrl", function ($scope, FilterFactory, $window) {
+    $scope.title = "Winterest";
+    $scope.search = FilterFactory;
+
+     $scope.top = () => {
+        $window.scrollTo(0, 0);
+    }
+});
